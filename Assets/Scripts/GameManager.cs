afc605da0a6147f9d137af2e1a63870bb20a4674
YBYCS <87234506+YBYCS@users.noreply.github.com>
2023-02-26 21:21:50 +0800

    地形生成阶段基本完成，接下来是生物群落

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -33,6 +33,7 @@ public class GameManager : Singleton<GameManager>
             }
             
         }
+        CreateAtlas();
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 //Show(mapTest[(i, 0)]);
@@ -41,11 +42,6 @@ public class GameManager : Singleton<GameManager>
         }
     }
 
-    void CreateGrass() {
-        var block = Instantiate(m_BlockPrefab);
-        var s = block.AddComponent<GrassBlock>();
-        s.Init(testData,0, 0, 0);
-    }
 
     MapData CreateMapData(int x,int y) {
         var d = new MapData();
@@ -54,18 +50,15 @@ public class GameManager : Singleton<GameManager>
         float max = 113;
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 16; j++) {
-                var h = 128 * Mathf.PerlinNoise((i+x*16) / max, (j+y*16) / max) + 64 * Mathf.PerlinNoise((i + x * 16) / max, (j + y * 16 )/ max);
-                //+ 32 * Mathf.PerlinNoise(i / 16, j / 16);
-                //+ 64 * Mathf.PerlinNoise( i / 16, j / 16) + 32 * Mathf.PerlinNoise( i / 16,  j / 16) ;
-                //Debug.Log(new Vector3(i,j,h));
-                //Debug.Log(new Vector2(4 * i, 4 * j));
+                var h = GetHeight(i + x * 16, j + y * 16);
 
-                //Debug.Log(h);
-                for (int k = 0; (k < h||k<64)&&k<256; k++) {
+                //注意K才是高度
+                for (int k = 0; (k <= h||k<64)&&k<256; k++) {
                     if (k < h) {
                         //改为泥土
-                        d.position[i, k, j] = 1;
+                        d.position[i, k, j] = GetBlockTest(k, (int)h);
                     }
+                    
                     else if(k == h) {
                         //并且如果在水下应该是泥土
                         if (h < 64) {
@@ -80,12 +73,81 @@ public class GameManager : Singleton<GameManager>
                         //海水
                         d.position[i, k, j] = 2;
                     }
-
                 }
             }
         }
         return d;
 
+    }
+    byte GetBlockTest(int h, int max) {
+        if (h <= 45) {
+            return 4;
+        }
+        else if (h >= max - 5) {
+            return 1;
+        }
+        else {
+            // 计算h在区间[45, max-5]中的比例
+            float proportion = (float)(h - 45) / (max - 50);
+
+            // 随机生成0到1之间的浮点数
+            float randomValue = UnityEngine.Random.value;
+
+            // 根据比例和随机数决定返回1或4
+            if (randomValue <= proportion) {
+                return 1;
+            }
+            else {
+                return 4;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 返回对应高度
+    /// </summary>
+    /// <param name="x">x位置</param>
+    /// <param name="y">y位置</param>
+    int GetHeight(float x, float y) {
+        int max = 6000000;
+        int send = 114514;
+        x += send;
+        y += send;
+        x /= max;
+        y /= max;
+        //基本频率
+        float bf = 30000;
+        float rate = 3;
+        var h = 128 * Mathf.PerlinNoise(bf * x%1, bf * y % 1) + 64 * Mathf.PerlinNoise(rate * bf*x % 1, rate * bf *y % 1) + 32 * Mathf.PerlinNoise(rate * rate * bf *x % 1, rate * rate * bf *y % 1);
+        //Debug.Log(h);
+        return (int)h;
+
+    }
+    Texture2D atlas;
+    Rect[] rects;
+    void CreateAtlas() {
+        //Texture2D[] textures;
+        atlas = new Texture2D(2048, 2048);
+        //textures = new Texture2D[2048];
+        List<Texture2D> textures = new();
+        textures.Add(Resources.Load<Texture2D>("Blocks/dirt"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/dirt"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/dirt"));
+
+        textures.Add(Resources.Load<Texture2D>("Blocks/water_overlay"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/water_overlay"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/water_overlay"));
+
+        textures.Add(Resources.Load<Texture2D>("Blocks/grass_top"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/grass_side"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/grass_side"));
+
+        textures.Add(Resources.Load<Texture2D>("Blocks/cobblestone"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/cobblestone"));
+        textures.Add(Resources.Load<Texture2D>("Blocks/cobblestone"));
+
+        rects = atlas.PackTextures(textures.ToArray(), 1, 2048);
+        atlas.filterMode = FilterMode.Point;
     }
 
 
@@ -94,23 +156,14 @@ public class GameManager : Singleton<GameManager>
         p.name = map.x + "-" + map.z;
         p.transform.position = new Vector3(map.x*16, 0, map.z*16);
         var filter = p.GetComponent<MeshFilter>();
-        var renderer = p.GetComponent<MeshRenderer>();
         Material material = p.GetComponent<MeshRenderer>().material;
-        Mesh mesh = new Mesh();
         int count = 0;
         List<Vector3> vertices = new();
         List<int> triangles = new();
         List<Vector2> uvs = new();
-        Texture2D[] textures;
-        textures = new Texture2D[2];
-        var tax = Resources.Load<Texture2D>("Blocks/dirt");
-        textures[0] = tax;
-        tax = Resources.Load<Texture2D>("Blocks/water_overlay");
-        textures[1] = tax;
-        Texture2D atlas = new Texture2D(2048, 2048);
+
         material.mainTexture = atlas;
-        Rect[] rects = atlas.PackTextures(textures, 1, 2048);
-        atlas.filterMode = FilterMode.Point;
+        
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 256; j++) {
                 for (int k = 0; k < 16; k++) {
@@ -185,8 +238,22 @@ public class GameManager : Singleton<GameManager>
         //var rect = rects[map.position[i, j, k]%3];
 
         //TODO
-        id = id>2?2:id;
         id--;
+        id *= 3;
+        switch (face) {
+            case Face.Top:
+                // 如果 face 为 Top，增量为 0
+                id += 0;
+                break;
+            case Face.Bottom:
+                // 如果 face 为 Bottom，增量为 2
+                id += 2;
+                break;
+            default:
+                // 其余情况下增量为 1
+                id += 1;
+                break;
+        }
         var rect = rects[id];
         uvs.Add(new Vector2(rect.xMin, rect.yMin));
         uvs.Add(new Vector2(rect.xMax, rect.yMin));
@@ -212,16 +279,16 @@ public class GameManager : Singleton<GameManager>
                 vertices[3] = new Vector3(-0.5f, -0.5f, 0.5f);
                 break;
             case Face.Back:
-                vertices[0] = new Vector3(0.5f, -0.5f, 0.5f);
-                vertices[1] = new Vector3(0.5f, 0.5f, 0.5f);
-                vertices[2] = new Vector3(-0.5f, 0.5f, 0.5f);
-                vertices[3] = new Vector3(-0.5f, -0.5f, 0.5f);
+                vertices[0] = new Vector3(-0.5f, -0.5f, 0.5f);
+                vertices[1] = new Vector3(0.5f, -0.5f, 0.5f);
+                vertices[2] = new Vector3(0.5f, 0.5f, 0.5f);
+                vertices[3] = new Vector3(-0.5f, 0.5f, 0.5f);
                 break;
             case Face.Front:
-                vertices[0] = new Vector3(-0.5f, 0.5f, -0.5f);
-                vertices[1] = new Vector3(0.5f, 0.5f, -0.5f);
-                vertices[2] = new Vector3(0.5f, -0.5f, -0.5f);
-                vertices[3] = new Vector3(-0.5f, -0.5f, -0.5f);
+                vertices[0] = new Vector3(0.5f, -0.5f, -0.5f);
+                vertices[1] = new Vector3(-0.5f, -0.5f, -0.5f);
+                vertices[2] = new Vector3(-0.5f, 0.5f, -0.5f);
+                vertices[3] = new Vector3(0.5f, 0.5f, -0.5f);
                 break;
             case Face.Left:
                 vertices[0] = new Vector3(-0.5f, -0.5f, -0.5f);
@@ -230,10 +297,10 @@ public class GameManager : Singleton<GameManager>
                 vertices[3] = new Vector3(-0.5f, 0.5f, -0.5f);
                 break;
             case Face.Right:
-                vertices[0] = new Vector3(0.5f, -0.5f, -0.5f);
-                vertices[1] = new Vector3(0.5f, 0.5f, -0.5f);
-                vertices[2] = new Vector3(0.5f, 0.5f, 0.5f);
-                vertices[3] = new Vector3(0.5f, -0.5f, 0.5f);
+                vertices[0] = new Vector3(0.5f, -0.5f, 0.5f);
+                vertices[1] = new Vector3(0.5f, -0.5f, -0.5f);
+                vertices[2] = new Vector3(0.5f, 0.5f, -0.5f);
+                vertices[3] = new Vector3(0.5f, 0.5f, 0.5f);
                 break;
         }
 
@@ -281,7 +348,7 @@ public class GameManager : Singleton<GameManager>
         if (y < 0) {
             return true;
         }
-        if (y > 256) {
+        if (y >= 256) {
             return true;
         }
         x %= 16;
@@ -289,6 +356,7 @@ public class GameManager : Singleton<GameManager>
         var d = GetMapData(i, j);
         if (d == null) return true;
         //Debug.Log(x + " " + y + " " + z);
+        //Debug.Log(new Vector3(x, y, z));
         if (d.position[x,y,z] != 0 ) {
             //string cn = "Block" + d.position[x, y, z];
             //Type type = Type.GetType(cn);  // 获取类型
